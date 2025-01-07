7ab42093eac5cff0ae29f95cdc091b93e839d562
euonymos <ilia@mlabs.city>
2025-01-07 14:59:08 -0600

    fix: add baeUrl to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import nextra from 'nextra'
 import remarkCodeImport from 'remark-code-import'
 
 const withNextra = nextra({
+  basePath: '/clb-docs',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   defaultShowCopyCode: true,
